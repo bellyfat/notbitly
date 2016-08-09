87fbbffb166993f1012d0f84d80717fbd5a53887
Leeor Baskin <lbaskin@gmail.com>
2016-08-09 16:07:11 -0400

    First working local version of app

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from urllib.parse import urlparse
+from urlshortener import *
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template(
+            'index.html',
+            short_url='')
+
+    elif request.method == 'POST':
+        long_url = request.form['url']
+        if urlparse(long_url).scheme == '':
+            long_url = 'http://' + long_url
+        url_id = insert_new_url(long_url)
+        encoded_url = dec_to_base(int(url_id))
+        return render_template(
+            'index.html',
+            short_url='http://localhost:5000/'+encoded_url
+            )
+
+@app.route('/<short_url>')
+def redirect_url(short_url):
+    decoded = to_base10(short_url)
+    print(decoded)
+    long_url = get_long_url(decoded)
+    return redirect(long_url)
+
+
+if __name__ == '__main__':
+      app.run(debug=True)
+
--- urlshortener.py
+++ urlshortener.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-
-import psycopg2
 import string
-
+import psycopg2
 
 def dec_to_base(num):
     alphabet = string.digits + string.ascii_letters + "$-_.+!*'(),"
@@ -23,5 +21,28 @@ def to_base10(num):
     return sum(alphabet.find(x)*len(alphabet)**i for i,x in enumerate(num))
 
 
-print(dec_to_base(17))
-print(to_base10('11'))
+# create table long_urls (id SERIAL PRIMARY KEY, url TEXT NOT NULL);
+
+
+def insert_new_url(url):
+    conn = psycopg2.connect(database='urls')
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO long_urls (url) VALUES ('%s') RETURNING id;''' % url)
+    last_id = cur.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    return last_id
+
+def get_long_url(url_id):
+    conn = psycopg2.connect(database='urls')
+    cur = conn.cursor()
+
+    cur.execute('''SELECT url FROM long_urls WHERE id = '%s';''' % url_id)
+    long_url = cur.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    return long_url
