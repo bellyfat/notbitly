1e1e6b14b9979568de6fce26dea5daf74b10a42f
Leeor Baskin <lbaskin@gmail.com>
2016-08-09 19:25:59 -0400

    Additional configuration for Heroku deployment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, redirect
 from urllib.parse import urlparse
+import os
 from urlshortener import *
 
 app = Flask(__name__)
@@ -16,23 +17,32 @@ def index():
 
     elif request.method == 'POST':
         long_url = request.form['url']
+
         if urlparse(long_url).scheme == '':
             long_url = 'http://' + long_url
         url_id = insert_new_url(long_url)
-        encoded_url = dec_to_base(int(url_id))
+
+        if 'HEROKU_URL' in os.environ:
+            host = os.environ['HEROKU_URL']
+        else:
+            host = 'http://localhost:5000/'
+        encoded_url = host + dec_to_base(int(url_id))
+
         return render_template(
             'index.html',
-            short_url='http://localhost:5000/'+encoded_url
+            short_url=encoded_url
             )
 
 @app.route('/<short_url>')
 def redirect_url(short_url):
     decoded = to_base10(short_url)
-    print(decoded)
     long_url = get_long_url(decoded)
     return redirect(long_url)
 
 
 if __name__ == '__main__':
-      app.run(debug=True)
+    if 'HEROKU_CHECK' in os.environ:
+        app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT')))
+    else:
+        app.run(debug=True)
 
--- urlshortener.py
+++ urlshortener.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 # -*- coding: utf-8 -*-
+import os
 import string
+import urllib
+from urllib.parse import urlparse
 import psycopg2
 
 def dec_to_base(num):
@@ -22,12 +25,9 @@ def to_base10(num):
 
     return sum(alphabet.find(x)*len(alphabet)**i for i,x in enumerate(num))
 
-
 # create table long_urls (id SERIAL PRIMARY KEY, url TEXT NOT NULL);
-
-
 def insert_new_url(url):
-    conn = psycopg2.connect(database='urls')
+    conn = in_heroku()
     cur = conn.cursor()
     cur.execute('''INSERT INTO long_urls (url) VALUES ('%s') RETURNING id;''' % url)
     last_id = cur.fetchone()[0]
@@ -38,7 +38,7 @@ def insert_new_url(url):
     return last_id
 
 def get_long_url(url_id):
-    conn = psycopg2.connect(database='urls')
+    conn = in_heroku()
     cur = conn.cursor()
 
     cur.execute('''SELECT url FROM long_urls WHERE id = '%s';''' % url_id)
@@ -48,3 +48,22 @@ def get_long_url(url_id):
     conn.close()
 
     return long_url
+
+def in_heroku():
+    if 'DATABASE_URL' in os.environ and os.environ['DATABASE_URL']:
+        urllib.parse.uses_netloc.append('postgres')
+        url = urlparse(os.environ['DATABASE_URL'])
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        return conn
+    else:
+        conn = psycopg2.connect(database='urls')
+        return conn
+
+    return False
